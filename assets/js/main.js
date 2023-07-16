a6a2abee17083b42ddf3eb10ca86f86b8d99e216
José Geraldo <jgt.josegeraldo@gmail.com>
2023-07-16 15:54:34 -0300

    Alteração largura font

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,10 +165,10 @@
 */
 
 setInterval(function () {
-	$(".backgroud-modal").animate({opacity:1},1500);
+	$(".backgroud-modal").animate({opacity:1},1000);
 
 	$(".backgroud-modal").on( "click", function() {
-		$(".backgroud-modal").animate({opacity:0}, 1500, function() {
+		$(".backgroud-modal").animate({opacity:0}, 500, function() {
 			$(".backgroud-modal").hide();
 		});
 	} );
