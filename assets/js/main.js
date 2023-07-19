f007658cfbf4f61b8f6ebd2a3b9172bba13fb04f
José Geraldo <jgt.josegeraldo@gmail.com>
2023-07-19 17:46:04 -0300

    Alteração time questão do efeito do popup de inscrição

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,10 +165,10 @@
 */
 
 setInterval(function () {
-	$(".backgroud-modal").animate({opacity:1},500);
+	$(".backgroud-modal").animate({opacity:1},750);
 
 	$(".backgroud-modal").on( "click", function() {
-		$(".backgroud-modal").animate({opacity:0}, 250, function() {
+		$(".backgroud-modal").animate({opacity:0}, 300, function() {
 			$(".backgroud-modal").hide();
 		});
 	} );
